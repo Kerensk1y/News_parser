b96b34cacb0cbd16d8f6dbfa44646ef83df60e52
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-07-27 17:45:18 +0300

    v 0.2.2
    РосдорНИИ полностью запарсил

--- main.py
+++ main.py
@@ -34,7 +34,6 @@ def rosavtodor():
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     parsed = str(soup.find_all("a", class_='boxLink')[0])
-    print(parsed, dataset)
     if parsed not in dataset:
         dataset.add(parsed)
         file_add(path, parsed)
@@ -43,7 +42,6 @@ def rosavtodor():
         description = post.find("div", class_="text").text.strip()
         url = soup.find_all('a', class_='boxLink')[0]
         link = URL + re.findall(r'href="(\/.*?)"', str(url))[0]
-        print(link, title, description)
         bot.send_message(channel_id, text=f"{link}\n{title}\n{description}")
 
 
@@ -72,29 +70,39 @@ def rosdornii_events():
 def rosdornii_news():
     path = "rosdornii_news.txt"
     dataset = file_set(path)
-    print(dataset)
     URL = "https://rosdornii.ru/press-center/news/"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     parsing = soup.findAll("div", class_="iblock-list-item-text w-md-60 pl-md-4")[0]
     parsed_link = re.findall(r'href="(\/.*?\/.*)"', str(parsing))[0]
-    print(parsed_link)
     if parsed_link not in dataset:
         dataset.add(parsed_link)
         file_add(path, parsed_link)
         title = parsing.find('a', hidefocus="true")
-        print(title)
         if title:
             title = title.get_text(strip=True)
-            print(title)
         parsed_link = "https://rosdornii.ru" + parsed_link
         bot.send_message(channel_id, text=f"{parsed_link}\n{title}")
+
+
+def rosdornii_digest():
+    path = "rosdornii_digest.txt"
+    dataset = file_set(path)
+    URL = "https://rosdornii.ru/press-center/digest/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    parsing = soup.findAll("p", class_="left t-1 my-2 t-title с-text-primary l-inherit l-hover-primary l-hover-underline-none transition")
+    parsed_link = re.findall(r'href="(\/.*?\/.*)">Дайджест новостей РФ \((\d\d\.\d\d\.\d\d\d\d)\).*', str(parsing))[0]
+    parsed_link = "https://rosdornii.ru" + parsed_link[0]
+    if parsed_link not in dataset:
+        file_add(path, parsed_link)
+        bot.send_message(channel_id, text=f"{parsed_link}")
 
 
 while True:
     rosavtodor()
     rosdornii_events()
     rosdornii_news()
+    rosdornii_digest()
+    print("Сплю... Проснусь через полчаса")
     time.sleep(1800)
-
-bot.polling()
