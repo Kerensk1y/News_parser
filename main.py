aa5c3fefd31a77a6e36ad6017023f4247f3a78cb
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-07-27 15:17:27 +0300

    v 0.2
    - Изменен способ отправки сообщений - убрал декораторы
    - Отпарсил РосдорНИИ

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from t0ken import *
 bot = telebot.TeleBot(API_KEY)
 print("run in progress...")
 
+
 def file_set(file_path):
     dataset = set()
     try:
@@ -22,17 +23,18 @@ def file_set(file_path):
 
 
 def file_add(file_path, data):
-    with open(file_path, 'a') as file:
+    with open(file_path, 'w') as file:
         file.write(f'{data}\n')
 
 
-def rosavtodor(message):
+def rosavtodor():
     path = "rosavtodor.txt"
     dataset = file_set(path)
     URL = "https://rosavtodor.gov.ru"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     parsed = str(soup.find_all("a", class_='boxLink')[0])
+    print(parsed, dataset)
     if parsed not in dataset:
         dataset.add(parsed)
         file_add(path, parsed)
@@ -45,37 +47,32 @@ def rosavtodor(message):
         bot.send_message(channel_id, text=f"{link}\n{title}\n{description}")
 
 
-'''
 def rosdornii():
     path = "rosdornii.txt"
     dataset = file_set(path)
     URL = "https://rosdornii.ru/press-center/event/"
+    # URL2 = "https://rosdornii.ru/press-center/news/" - создать список и ебнуть фор
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    base = soup.findAll("div", class_="NewsCalNews")
-    parsed = base[-1]
-    # создаю базу url'ей со страницы
-    base = re.findall(r'href="(\/.*?\/.*)"', str(base))
-    # достаю инфу из последнего поста
-    title = parsed.find("a", class_="t--1 mb-2 sf-link sf-link-theme c-text-primary").text.strip()
-    url = parsed.find("a", class_="t--1 mb-2 sf-link sf-link-theme c-text-primary")
-    url = "https://rosdornii.ru" + url.get("href")
-    # достаю дату
-    date = soup.find_all("a", class_="events btn-primary")[-1]
-    dates = re.findall(r'date=(\d\d\.\d\d\.\d\d\d\d)', str(date))
-    if dates:
-        date = dates[0]
-
-    return date, url, title
-
-'''
+    parsing = soup.findAll("div", class_="NewsCalNews")[-1]
+    parsed = re.findall(r'href="(\/.*?\/.*)"', str(parsing))
+    if parsed[0] not in dataset:
+        dataset.add(parsed[0])
+        file_add(path, parsed[0])
+        title = parsing.find("a", class_="t--1 mb-2 sf-link sf-link-theme c-text-primary").text.strip()
+        url = parsing.find("a", class_="t--1 mb-2 sf-link sf-link-theme c-text-primary")
+        link = "https://rosdornii.ru" + url.get("href")
+        # достаю дату
+        date = soup.find_all("a", class_="events btn-primary")[-1]
+        dates = re.findall(r'date=(\d\d\.\d\d\.\d\d\d\d)', str(date))
+        if dates:
+            date = dates[0]
+        bot.send_message(channel_id, text=f"{link}\n{title}\n{date}")
 
 
-@bot.message_handler(commands=['start'])
-def commands(message):
-    while True:
-        rosavtodor(message)
-        time.sleep(1800)
-
+while True:
+    rosavtodor()
+    rosdornii()
+    time.sleep(1800)
 
 bot.polling()
