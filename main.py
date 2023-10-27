1c966842aa72bca3b54a7b161927c716a3e1330e
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-10-27 11:25:19 +0300

    v. 1.1.5
    functions renamed + added 1 more exception for the code stability

--- main.py
+++ main.py
@@ -13,14 +13,13 @@ logging.basicConfig(filename='logger.log', level=logging.INFO,
                     format='%(levelname)s (%(asctime)s): %(message)s (Line: %(lineno)d) [%(filename)s]',
                     datefmt='%d/%m/%Y %H:%M:%S', encoding="utf-8", filemode="w")
 
-
 bot = telebot.TeleBot(API_KEY)
 
 logging.info('Bot is running...')
 
 
 # Функция забирает данные о последней выгруженной новости в переменную. Если данных нет создает пустой файл
-def file2set(file_path):
+def file_to_str(file_path):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             data = file.read()
@@ -30,8 +29,9 @@ def file2set(file_path):
             file.write('')
         return ''
 
+
 # Функция перезаписывает соответствующий файл, содержащий идентификатор последней отправленнй новости
-def add2file(file_path, data):
+def write_to_file(file_path, data):
     with open(file_path, 'w') as file:
         file.write(f'{data}\n')
 
@@ -40,9 +40,9 @@ def add2file(file_path, data):
 def send_if_upd(path, title, link, id):
     try:
         global is_sent
-        dataset = file2set(path)
+        dataset = file_to_str(path)
         if id not in dataset:
-            add2file(path, id)
+            write_to_file(path, id)
             bot.send_message(channel_id, text=f'<a href="{link}">{title}</a>', parse_mode='html')
             is_sent = True
             logging.info(f'New message on {path}')
@@ -71,12 +71,16 @@ site_process = {
 
 
 def parse_site(url):
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    try:
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, "html.parser")
 
-    process_func, path = site_process[url]
-    title, link, id = process_func(soup, url)
-    send_if_upd(path, title, link, id)
+        process_func, path = site_process[url]
+        title, link, id = process_func(soup, url)
+        send_if_upd(path, title, link, id)
+    except Exception as e:
+        logging.error(f"Ошибка при парсинге страницы {url}: {str(e)}")
+        time.sleep(5)
 
 
 # Функция, содержащая петлю, выполняющую весь парсинг
@@ -96,7 +100,7 @@ def poll():
         try:
             bot.polling(none_stop=True)
         except Exception as e:
-            logging.error(f"Ошибка при запуске polling: {e}")
+            logging.error(f"Ошибка при запуске polling: {str(e)}")
             time.sleep(5)
 
 
