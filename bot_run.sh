db8961c3b557cd53a6769df8f4e902dae3f778dc
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-11-17 14:10:14 +0300

    Create bot_run.sh

---
+++ bot_run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+LOG_FILE="bot_log.txt"
+
+echo "Bot started at: $(date)" >> "$LOG_FILE"
+
+while true; do
+    {
+        python3 ~/News_parser/main.py
+        echo "---------------------------"
+        echo "Bot crashed at: $(date)"
+        echo "Restarting in 10 seconds..."
+    } >> "$LOG_FILE"
+    sleep 10
+done
