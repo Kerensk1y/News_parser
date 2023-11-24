78597380a91e9e223e288a085f79635d45a78126
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-11-24 16:09:58 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@ read -p "Enter your bot API key: " api_key
 ch_id="USER INPUT"
 read -p "Enter your channel id: " ch_id
 
-echo -e "API_KEY='$api_key'\nchannel_id='$ch_id'" > t0ken.py
+echo -e "API_KEY='$api_key'\nchannel_id='$ch_id'" > ~/news_parser/t0ken.py
 
 mkdir -p news_parser/db
 
-chmod +x bot_run.sh
+chmod +x ~/news_parser/bot_run.sh
