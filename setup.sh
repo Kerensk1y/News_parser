b4c65386c5c7c02d8b895f4f0f588194f846d58e
RomanovKR <73065648+Kerensk1y@users.noreply.github.com>
2023-11-30 16:42:57 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ read -p "Enter your channel id: " ch_id
 
 echo -e "API_KEY='$api_key'\nchannel_id='$ch_id'" > ~/news_parser/t0ken.py
 
-mkdir -p news_parser/db
+mkdir -p ~/news_parser/db
 
 chmod +x ~/news_parser/bot_run.sh
+chmod +x ~/news_parser/bot_stop.sh
